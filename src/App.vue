1c001aa30b35e6a0a4342021bae37ac740cfa59a
kees jansen <kees.jansen@assetrisk.com>
2023-08-01 15:11:53 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <Area />
+  </div>
+</template>
+
+<script>
+  import Area from 'arc_charts';
+
+  export default {
+    name: 'app',
+    components: {
+      Area,
+    },
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
